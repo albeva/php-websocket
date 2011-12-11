89bf7e9259a4171d4a3abc1469dfd05880366ff7
Albert Varaksin <albeva@me.com>
2011-12-11 00:35:21 +0000

    Chat example WIP

--- examples/chat/server.php
+++ examples/chat/server.php
@@ -20,10 +20,37 @@ $autoloader->register();
 class Chatter extends WebSocket\Client
 {
 	/**
-	 * @var string containing the name of the connected user
+	 * connected clients
+	 *
+	 * @var array
 	 */
-	public $name = 'User';
-	
+	static $clients = array();
+
+	/**
+	 * Notify the client of its ID
+	 */
+	function onConnected()
+	{
+		$this->send(array(
+			'action' => 'notify',
+			'id' => $this->id,
+			'clients' => array_values(self::$clients)
+		));
+	}
+
+
+	/**
+	 * Notify others
+	 */
+	function onDisconnected()
+	{
+		unset(self::$clients[$this->id]);
+		$this->sendAll(array(
+			'action' => 'disconnect',
+			'id' => $this->id
+		));
+	}
+
 	/**
 	 * Receive message from the client
 	 *
@@ -31,24 +58,20 @@ class Chatter extends WebSocket\Client
 	 */
 	function receive($msg)
 	{
-		if (isset($msg['name'])) {
-			$this->name = $msg['name'];
-			return;
+		$msg['id'] = $this->id;
+		if (in_array($msg['action'], array('connect', 'update'))) {
+			$copy = $msg;
+			unset($copy['action']);
+			self::$clients[$this->id] = $copy;
 		}
-
-		// send to all except self
-		$this->sendAll(array(
-			'user'		=> $this->name,
-			'id'		=> $this->getId(),
-			'message'	=> $msg['message']
-		));
+		$this->sendAll($msg);
 	}
 }
 
 // Create server
 try {
 	$server = new WebSocket\Server(array(
-		'host'			=> 'localhost',
+		'host'			=> '173.203.111.216',
 		'port'			=> 12345,
 		'clientClass'	=> 'Chatter',
 		'serializer'	=> 'json'
